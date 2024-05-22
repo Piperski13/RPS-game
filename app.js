19f0a0abca901d876a8fa27e8af63ca13004e379
Aleksa Piperski <mr.alexpiperski@gmail.com>
2024-05-22 19:12:01 +0200

    Version 4(working backend for storing score.json)

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json()); //middleware
 app.use(cors()); //middleware
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 let score = JSON.parse(fs.readFileSync('./backend/score.json','utf-8'));
 
@@ -19,7 +22,9 @@ app.post('/score',(req,res)=>{
   });
   
 });
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const port=3000;
 app.listen(port,()=>{
